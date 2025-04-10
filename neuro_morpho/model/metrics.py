0a82b4ce8c4783d79b42a15ef1223b449b71c898
Ryan Hausen <ryan.hausen@gmail.com>
2025-04-10 10:24:01 -0400

    fix for metrics

--- neuro_morpho/model/metrics.py
+++ neuro_morpho/model/metrics.py
@@ -1,15 +1,15 @@
 from typing import Callable
 
 import gin
-import numpy as np
+import torch
 
-pred = np.ndarray
-lbl = np.ndarray
-METRIC_FN = Callable[[pred, lbl], tuple[str, float]]
+PRED = torch.Tensor
+LBL = torch.Tensor
+METRIC_FN = Callable[[PRED, LBL], tuple[str, float]]
 
 
 @gin.configurable(allowlist=["threshold"])
-def accuracy(pred: pred, lbl: lbl, threshold: float) -> tuple[str, float]:
+def accuracy(pred: PRED, lbl: LBL, threshold: float) -> tuple[str, float]:
     """Calculate the accuracy of predictions.
 
     Args:
@@ -18,15 +18,15 @@ def accuracy(pred: pred, lbl: lbl, threshold: float) -> tuple[str, float]:
     Returns:
         float: The accuracy as a percentage.
     """
-    pred_binary = (pred >= threshold).astype(int)
-    correct_predictions = np.sum(pred_binary == lbl)
+    pred_binary = (pred >= threshold).int()
+    correct_predictions = torch.sum(pred_binary == lbl)
     total_predictions = len(lbl)
     accuracy_value = correct_predictions / total_predictions
     return "accuracy", accuracy_value  # Return as percentage
 
 
 @gin.configurable(allowlist=["class_idx", "threshold"])
-def class_accuracy(pred: pred, lbl: lbl, class_idx: int, threshold: float) -> tuple[str, float]:
+def class_accuracy(pred: PRED, lbl: LBL, class_idx: int, threshold: float) -> tuple[str, float]:
     """Calculate the class-wise accuracy of predictions.
 
     Args:
