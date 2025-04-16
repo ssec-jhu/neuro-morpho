94bf10708127f5a0fb4d53235cbccb1dc699a238
Ryan Hausen <ryan.hausen@gmail.com>
2025-04-16 16:53:14 -0400

    tests for ci/cd

--- neuro_morpho/model/loss.py
+++ neuro_morpho/model/loss.py
@@ -42,18 +42,17 @@ LOSS_FN = Callable[[PRED, TARGET], tuple[NAME_LOSS, ...]]
 class WeightedFocalLoss(torch.nn.Module):
     "Non weighted version of Focal Loss"
 
-    def __init__(self, alpha=0.25, gamma=2, coefs: list[float] = None):
+    def __init__(self, beta=0.25, gamma=2, coefs: list[float] = None):
         super(WeightedFocalLoss, self).__init__()
-        self.alpha = torch.tensor(alpha).cuda()
+        self.beta = torch.tensor(beta).cuda()
         self.gamma = gamma
         self.coefs = coefs
 
     def forward(self, inputs, targets) -> tuple[str, torch.Tensor]:
         BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction="none")
         targets = targets.type(torch.long)
-        # at = self.alpha.gather(0, targets.data.view(-1))
         pt = torch.exp(-BCE_loss)
-        F_loss = self.alpha * (1 - pt) ** self.gamma * BCE_loss
+        F_loss = self.beta * (1 - pt) ** self.gamma * BCE_loss
         return "weighted_focal_loss", F_loss.mean()
 
 
---
+++ neuro_morpho/model/tests/test_loss.py
@@ -0,0 +1,6 @@
+
+import torch
+
+from neuro_morpho.model import loss
+
+def test_weighted_focal_loss():
