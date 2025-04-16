7b91d102c5828a6081a915a5f4c1076d2a1e0823
Ryan Hausen <ryan.hausen@gmail.com>
2025-04-16 13:11:38 -0400

    added tests for comet logging

--- neuro_morpho/logging/tests/test_comet.py
+++ neuro_morpho/logging/tests/test_comet.py
@@ -5,7 +5,7 @@ import numpy as np
 from neuro_morpho.logging.comet import CometLogger
 
 
-def test_comet_logger_log_scalar():
+def test_log_scalar():
     """Test the initialization of CometLogger."""
     logger = CometLogger(
         api_key=None,
@@ -26,7 +26,7 @@ def test_comet_logger_log_scalar():
     assert logger.experiment.metrics["test_metric"] == 0.5
 
 
-def test_comet_logger_log_triplet():
+def test_log_triplet():
     """Test the initialization of CometLogger."""
     logger = CometLogger(
         api_key=None,
@@ -52,4 +52,39 @@ def test_comet_logger_log_triplet():
         train=True,
     )
 
+    assert logger.experiment is not None
+
+
+def test_log_parameters():
+    """Test the initialization of CometLogger."""
+    logger = CometLogger(
+        api_key=None,
+        project_name="test_project",
+        workspace="test_workspace",
+        auto_param_logging=True,
+        auto_metric_logging=True,
+        disabled=True,
+    )
+
+    metrics = {"learning_rate": 0.001, "batch_size": 32}
+    logger.log_parameters(metrics)
+
+    assert logger.experiment is not None
+    assert logger.experiment.params == metrics
+
+
+def test_log_code():
+    """Test the initialization of CometLogger."""
+    logger = CometLogger(
+        api_key=None,
+        project_name="test_project",
+        workspace="test_workspace",
+        auto_param_logging=True,
+        auto_metric_logging=True,
+        disabled=True,
+    )
+
+    # this currently doesn't do anything because comet experiments that disable
+    # logging don't do anything with code
+    logger.log_code(folder=".")
     assert logger.experiment is not None
