d3ee8a167e40f0286cd6a9bfaec469ae993afa0e
Ryan Hausen <ryan.hausen@gmail.com>
2025-04-19 16:09:56 -0400

    added mroe tests

--- neuro_morpho/model/tests/test_unet.py
+++ neuro_morpho/model/tests/test_unet.py
@@ -1,12 +1,43 @@
 """Tests for the UNet model."""
 
+from collections.abc import Callable
 from pathlib import Path
+from typing import Any
 
 import numpy as np
 import pytest
 import torch
 
-from neuro_morpho.model.unet import UNet
+from neuro_morpho.model import unet
+
+
+@pytest.mark.parametrize(
+    ("fn", "inputs", "expected_output"),
+    [
+        (lambda x: 2 * x, 2, 4),
+        (lambda x: 2 * x, (2, 2), (4, 4)),
+    ],
+)
+def test_apply_tpl(
+    fn: Callable,
+    inputs: Any | tuple[Any, ...],
+    expected_output: Any | tuple[Any, ...],
+) -> None:
+    """Test the apply_tpl method of the UNet model."""
+    assert expected_output == unet.apply_tpl(fn, inputs)
+
+
+def test_cast_and_move():
+    x = torch.tensor([1, 2, 3], dtype=torch.int32)
+    x = unet.cast_and_move(x, device="cpu")
+    assert x.dtype == torch.float32
+
+
+def test_detach_and_move():
+    inputs = torch.tensor([[1, 2, 3], [3, 4, 5]], dtype=torch.int32)
+    outputs = unet.detach_and_move(inputs)
+    np.testing.assert_equal(inputs.numpy(), outputs)
+    np.testing.assert_equal(inputs.numpy()[0], unet.detach_and_move(inputs, 0))
 
 
 def test_shapes():
@@ -17,7 +48,7 @@ def test_shapes():
     input_tensor = np.random.rand(1, 1, 256, 256).astype(np.float32)
 
     # Initialize the UNet model
-    model = UNet(
+    model = unet.UNet(
         n_input_channels=1,
         n_output_channels=1,
         encoder_channels=[64, 128, 256, 512, 1024],
@@ -30,7 +61,7 @@ def test_shapes():
 
 def test_predict_dir_raises():
     """Test that predict_dir raises an error when called."""
-    model = UNet(
+    model = unet.UNet(
         n_input_channels=1,
         n_output_channels=1,
         encoder_channels=[64, 128, 256, 512, 1024],
@@ -43,7 +74,7 @@ def test_predict_dir_raises():
 
 def test_save(tmp_path: Path):
     """Test saving the model."""
-    model = UNet(
+    model = unet.UNet(
         n_input_channels=1,
         n_output_channels=1,
         encoder_channels=[64, 128, 256, 512, 1024],
@@ -58,7 +89,7 @@ def test_save(tmp_path: Path):
 def test_load(tmp_path: Path):
     """Test loading the model."""
     # This test needs to be refined, to maybe check that the state_dict is the same after loading.
-    model = UNet(
+    model = unet.UNet(
         n_input_channels=1,
         n_output_channels=1,
         encoder_channels=[64, 128, 256, 512, 1024],
@@ -68,11 +99,11 @@ def test_load(tmp_path: Path):
     model.save(tmp_path)
     model_path = tmp_path / "model.pt"
 
-    model_2 = UNet(
+    model_2 = unet.UNet(
         n_input_channels=1,
         n_output_channels=1,
         encoder_channels=[64, 128, 256, 512, 1024],
         decoder_channels=[512, 256, 128, 64],
     )
     model_2.load(model_path)
-    assert isinstance(model_2, UNet)
+    assert isinstance(model_2, unet.UNet)
