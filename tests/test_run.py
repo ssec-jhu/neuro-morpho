a75ff3c87e1e7b54c40374a4dfe142823196bec9
Vladimir Kluzner <vkluzner@jhu.edu>
2025-06-18 18:40:54 -0700

    Refactor test_run_infer

--- tests/test_run.py
+++ tests/test_run.py
@@ -11,53 +11,41 @@ from neuro_morpho.util import get_device
 def test_run_infer():
     """Test the run function in the inference mode."""
 
-    # Initialize config params
-    run.train = False
-    run.infer = True
-    run.model = UNet(
+    # Initialize the UNet model
+    unet_model = UNet(
         n_input_channels=1,
         n_output_channels=1,
         encoder_channels=[64, 128, 256, 512, 1024],
         decoder_channels=[512, 256, 128, 64],
         device=get_device(),
     )
-    run.model_file = None
-    run.training_x_dir = "data/processed/train/imgs/"
-    run.training_y_dir = "data/processed/train/lbls/"
-    run.testing_x_dir = "data/processed/test/"
-    run.testing_y_dir = "data/processed/test/lbls/"
-    run.model_save_dir = "models/"
-    run.model_out_y_dir = "data/output/"
-    run.model_stats_output_dir = "data/stats/model/"
-    run.labeled_stats_output_dir = "data/stats/label/"
-    run.report_output_dir = "data/report/"
-    run.tile_size = 512
-    run.tile_assembly = "nn"
-    run.logger = None
+
+    testing_x_dir = "data/processed/test/"
+    model_out_y_dir = "data/output/"
 
     # Create a dummy input image with the shape (batch_size, height, width, channels)
     input_tensor = np.random.rand(1, 1, 256, 256).astype(np.float32)
-    Path(run.testing_x_dir).mkdir(parents=True, exist_ok=True)
-    cv2.imwrite(run.testing_x_dir + "test_img.tif", (input_tensor[0, 0, :, :] * 255).astype(np.uint8))
+    Path(testing_x_dir).mkdir(parents=True, exist_ok=True)
+    cv2.imwrite(testing_x_dir + "test_img.tif", (input_tensor[0, 0, :, :] * 255).astype(np.uint8))
 
     run.run(
-        model=run.model,
-        model_file=run.model_file,
-        training_x_dir=run.training_x_dir,
-        training_y_dir=run.training_y_dir,
-        testing_x_dir=run.testing_x_dir,
-        testing_y_dir=run.testing_y_dir,
-        model_save_dir=run.model_save_dir,
-        model_out_y_dir=run.model_out_y_dir,
-        model_stats_output_dir=run.model_stats_output_dir,
-        labeled_stats_output_dir=run.labeled_stats_output_dir,
-        report_output_dir=run.report_output_dir,
-        logger=run.logger,
-        train=run.train,
-        infer=run.infer,
-        tile_size=run.tile_size,
-        tile_assembly=run.tile_assembly,
+        model=unet_model,
+        model_file=None,
+        training_x_dir="data/processed/train/imgs/",
+        training_y_dir="data/processed/train/lbls/",
+        testing_x_dir=testing_x_dir,
+        testing_y_dir="data/processed/test/lbls/",
+        model_save_dir="models/",
+        model_out_y_dir=model_out_y_dir,
+        model_stats_output_dir="data/stats/model/",
+        labeled_stats_output_dir="data/stats/label/",
+        report_output_dir="data/report/",
+        logger=None,
+        train=False,
+        infer=True,
+        tile_size=512,
+        tile_assembly="nn",
     )
 
-    assert Path(run.model_out_y_dir).exists()
-    assert (Path(run.model_out_y_dir) / Path("test_img_pred.tif")).exists()
+    assert Path(model_out_y_dir).exists()
+    assert (Path(model_out_y_dir) / Path("test_img_pred.tif")).exists()
