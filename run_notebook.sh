9c171c579c83de7627962f851372f451d8a1062b
Vladimir Kluzner <vkluzner@jhu.edu>
2025-07-29 09:34:14 -0400

    Remove and ignore shell scripts

--- run_notebook.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source activate py312
-pip install pathlib tqdm concurrent papermill --upgrade
-papermill neuro_morpho/notebooks/data_organizer.ipynb neuro_morpho/notebooks/output.ipynb
