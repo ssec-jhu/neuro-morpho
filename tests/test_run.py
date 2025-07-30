29f42985dbc90d2d556ea012a3f032283966f471
Vladimir Kluzner <vkluzner@jhu.edu>
2025-07-30 15:49:29 -0400

    Extend test coverage

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,8 +1,10 @@
 import itertools
+import shutil
 from pathlib import Path
 
 import cv2
 import numpy as np
+import pytest
 
 from neuro_morpho import run
 from neuro_morpho.logging.comet import CometLogger
@@ -32,10 +34,11 @@ def test_run():
 
     logger = CometLogger(experiment_key=model_id, disabled=True)
 
-    testing_dir = Path("data/processed/test")
     training_dir = Path("data/processed/train")
+    validating_dir = Path("data/processed/val")
+    testing_dir = Path("data/processed/test")
     model_out_y_dir = Path("data/output") / Path(model_id)
-    for dir_path, subdir in itertools.product([testing_dir, training_dir], ["test_imgs", "test_lbls"]):
+    for dir_path, subdir in itertools.product([training_dir, validating_dir, testing_dir], ["test_imgs", "test_lbls"]):
         (dir_path / subdir).mkdir(parents=True, exist_ok=True)
 
     input_tensor = np.zeros((256, 256), dtype=np.float32)
@@ -46,18 +49,20 @@ def test_run():
     half_window = 2  # because 4x4 window -> 2 pixels in each direction from center
     input_tensor[center_y - half_window : center_y + half_window, center_x - half_window : center_x + half_window] = 0.0
     target_tensor = input_tensor.copy()
-    cv2.imwrite(testing_dir / "test_lbls" / "test_lbl.tif", (target_tensor * 255).astype(np.uint8))
-
-    # Apply Gaussian blur
-    input_tensor = cv2.GaussianBlur(input_tensor, ksize=(3, 3), sigmaX=1)
+    input_tensor = cv2.GaussianBlur(input_tensor, ksize=(3, 3), sigmaX=1)  # Apply Gaussian blur
+    cv2.imwrite(training_dir / "test_imgs" / "test_img.tif", (input_tensor * 255).astype(np.uint8))
+    cv2.imwrite(training_dir / "test_lbls" / "test_lbl.tif", (target_tensor * 255).astype(np.uint8))
+    cv2.imwrite(validating_dir / "test_imgs" / "test_img.tif", (input_tensor * 255).astype(np.uint8))
+    cv2.imwrite(validating_dir / "test_lbls" / "test_lbl.tif", (target_tensor * 255).astype(np.uint8))
     cv2.imwrite(testing_dir / "test_imgs" / "test_img.tif", (input_tensor * 255).astype(np.uint8))
+    cv2.imwrite(testing_dir / "test_lbls" / "test_lbl.tif", (target_tensor * 255).astype(np.uint8))
 
     run.run(
         model=unet_model,
-        training_x_dir=training_dir / "train_imgs",
-        training_y_dir=training_dir / "train_lbls",
-        validating_x_dir=testing_dir / "test_imgs",
-        validating_y_dir=testing_dir / "test_lbls",
+        training_x_dir=training_dir / "test_imgs",
+        training_y_dir=training_dir / "test_lbls",
+        validating_x_dir=validating_dir / "test_imgs",
+        validating_y_dir=validating_dir / "test_lbls",
         testing_x_dir=testing_dir / "test_imgs",
         testing_y_dir=testing_dir / "test_lbls",
         model_save_dir=model_save_dir,
@@ -76,13 +81,14 @@ def test_run():
     assert (Path(model_out_y_dir) / Path("test_img_pred.tif")).exists()
     assert (Path(model_out_y_dir) / Path("test_img_pred_bin.tif")).exists()
     assert (Path(model_out_y_dir) / Path("test_img_pred_bin_fixed.tif")).exists()
+    shutil.rmtree(model_out_y_dir)
 
     run.run(
         model=unet_model,
         training_x_dir=training_dir / "train_imgs",
         training_y_dir=training_dir / "train_lbls",
-        validating_x_dir=testing_dir / "test_imgs",
-        validating_y_dir=testing_dir / "test_lbls",
+        validating_x_dir=validating_dir / "test_imgs",
+        validating_y_dir=validating_dir / "test_lbls",
         testing_x_dir=testing_dir / "test_imgs",
         testing_y_dir=testing_dir / "test_lbls",
         model_save_dir=model_save_dir,
@@ -101,3 +107,25 @@ def test_run():
     assert (Path(model_out_y_dir) / Path("test_img_pred.tif")).exists()
     assert (Path(model_out_y_dir) / Path("test_img_pred_bin.tif")).exists()
     assert (Path(model_out_y_dir) / Path("test_img_pred_bin_fixed.tif")).exists()
+    shutil.rmtree(model_out_y_dir)
+
+    with pytest.raises(ValueError, match="Logger is not provided. Please provide a logger to log the results."):
+        run.run(
+            model=unet_model,
+            training_x_dir=training_dir / "train_imgs",
+            training_y_dir=training_dir / "train_lbls",
+            validating_x_dir=validating_dir / "test_imgs",
+            validating_y_dir=validating_dir / "test_lbls",
+            testing_x_dir=testing_dir / "test_imgs",
+            testing_y_dir=testing_dir / "test_lbls",
+            model_save_dir=model_save_dir,
+            model_out_y_dir=model_out_y_dir,
+            model_stats_output_dir="data/stats/model/",
+            labeled_stats_output_dir="data/stats/label/",
+            report_output_dir="data/report/",
+            logger=None,
+            train=False,
+            get_threshold=True,
+            test=False,
+            infer=True,
+        )
