221393a601df8f496caf13722c7e8014a41b9f58
Vladimir Kluzner <vkluzner@jhu.edu>
2025-08-01 15:28:01 -0400

    Bug fix for validation module

--- neuro_morpho/model/unet.py
+++ neuro_morpho/model/unet.py
@@ -344,8 +344,9 @@ class UNet(base.BaseModel):
                         y=y,
                     )
                     x = detach_and_move(x, idx=0 if isinstance(x, tuple | list) else None)
-                    pred = torch.sigmoid(detach_and_move(pred, idx=0 if isinstance(pred, tuple | list) else None))
                     y = detach_and_move(y, idx=0 if isinstance(y, tuple | list) else None)
+                    pred = detach_and_move(pred, idx=0 if isinstance(pred, tuple | list) else None)
+                    pred = 1 / (1 + np.exp(-pred))  # Sigmoid activation
 
                     loss = sum(map(lambda lss: lss[1], losses)) if isinstance(losses, (tuple, list)) else losses[1]
                     metrics_values = [fn(pred, y) for fn in metric_fns]
